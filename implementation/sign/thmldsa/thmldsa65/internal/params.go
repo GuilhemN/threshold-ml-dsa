484a1ea5c03a9ebc02c056e16ec9244bcf74d750
Sofía Celi <cherenkovd69@gmail.com>
2025-12-05 18:09:54 +0000

    Add 65

---
+++ implementation/sign/thmldsa/thmldsa65/internal/params.go
@@ -0,0 +1,20 @@
+// Code generated from params.templ.go. DO NOT EDIT.
+
+package internal
+
+const (
+	Name          = "ML-DSA-65"
+	K             = 6
+	L             = 5
+	Eta           = 4
+	DoubleEtaBits = 4
+	Omega         = 55
+	Tau           = 49
+	Gamma1Bits    = 19
+	Gamma2        = 261888
+	NIST          = true
+	TRSize        = 64
+	CTildeSize    = 48
+	B 	      = 221116.151669661 // FIX
+	B0            = 221041.3274003604 // FIX
+)
